147a316e7e41ba918fe21b9d9d9fb7d65c757c1c
agyat <shubhamcp23@gmail.com>
2025-07-13 00:08:58 +0530

    fix

--- utils/step2.js
+++ utils/step2.js
@@ -19,14 +19,14 @@ async function sendLeaveConfirmationAndMenu({ phoneNumberId, to, date }) {
           {
             type: 'reply',
             reply: {
-              id:    '01set_full_day_leave',
+              id:    '01_set_full_day_leave',
               title: 'Set Full day Leave'
             }
           },
           {
             type: 'reply',
             reply: {
-              id:    '01set_partial_leave',
+              id:    '01_set_partial_leave',
               title: 'Set Partial Leave'
             }
           }
